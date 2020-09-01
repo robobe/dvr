92aa63ee914a96d9380ddd2cf9d749c1bdabe879
robo <robo>
2020-09-01 21:38:51 +0300

    errors handlers

--- examples/gst/hello1.py
+++ examples/gst/hello1.py
@@ -27,41 +27,54 @@ def on_message(bus: Gst.Bus, message: Gst.Message, loop: GObject.MainLoop):
     elif mtype == Gst.MessageType.WARNING:
         err, debug = message.parse_warning()
         print(err, debug)
-
+    elif mtype == Gst.MessageType.STATE_CHANGED:
+        print(message.parse_state_changed())
+    else:
+        print(mtype)
     return True
+
+def on_status_changed(bus, message):
+        print('status_changed message -> {}'.format(message))
+
+def on_eos(bus, message):
+    print('eos message -> {}'.format(message))
+
+def on_info(bus, message):
+    print('info message -> {}'.format(message))
+
+def on_error(bus, message):
+    print('error message -> {}'.format(message.parse_error()))
 
 
 # Gst.Pipeline https://lazka.github.io/pgi-docs/Gst-1.0/classes/Pipeline.html
-pipeline = Gst.Pipeline()
+pipe="v4l2src device=/dev/video2 ! videoconvert ! autovideosink"
+pipeline = Gst.parse_launch(pipe)
 
-# Creates element by name
-# https://lazka.github.io/pgi-docs/Gst-1.0/classes/ElementFactory.html#Gst.ElementFactory.make
-src_name = "my_video_test_src"
-src = Gst.ElementFactory.make("videotestsrc", "my_video_test_src")
-src.set_property("num-buffers", 50)
-src.set_property("pattern", "ball")
 
-sink = Gst.ElementFactory.make("autovideosink")
-
-pipeline.add(src, sink)
-
-src.link(sink)
 
 # https://lazka.github.io/pgi-docs/Gst-1.0/classes/Bus.html
 bus = pipeline.get_bus()
 
 # allow bus to emit messages to main thread
 bus.add_signal_watch()
+bus.connect('message::error', on_error)
+bus.connect('message::state-changed', on_status_changed)
+bus.connect('message::eos', on_eos)
+bus.connect('message::info', on_info)
+bus.enable_sync_message_emission()
+# bus.connect('sync-message::element', set_frame_handle)
 
 # Start pipeline
 pipeline.set_state(Gst.State.PLAYING)
 
 # Init GObject loop to handle Gstreamer Bus Events
-loop = GObject.MainLoop()
 
+
+loop = GObject.MainLoop()
+# bus.connect("message", on_message, loop)
 # Add handler to specific signal
 # https://lazka.github.io/pgi-docs/GObject-2.0/classes/Object.html#GObject.Object.connect
-bus.connect("message", on_message, loop)
+
 
 try:
     loop.run()

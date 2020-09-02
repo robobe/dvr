2c2c3f5558fae0aa3dce103a20c73f9a85c3d941
robo <robo>
2020-09-02 21:13:22 +0300

    switch

--- src/cap.py
+++ src/cap.py
@@ -2,19 +2,37 @@ import threading
 import time
 import numpy as np
 import cv2
+import traceback
 from collections import deque
 
 
 class camera(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.__cap = cv2.VideoCapture("/dev/video2")
+        self.__cap = None
         self.__q = deque(maxlen=1)
+        self.__tmp_img = np.ones([480,640,3],dtype=np.uint8)
 
     def run(self):
-        while(True):
-            ret, frame = self.__cap.read()
-            self.__q.append(frame)
+        name = "/dev/video2"
+        while True:
+            try:
+                self.__cap = cv2.VideoCapture(name)
+                try:
+                    while(True):
+                        ret, frame = self.__cap.read()
+                        if not ret:
+                            frame = self.__tmp_img
+                        self.__q.append(frame)
+                except:
+                    print(traceback.format_exc())
+                    self.__cap.release()
+                time.sleep(1)
+            except:
+                print(traceback.format_exc())
+                time.sleep(1)
+                name = "/dev/video3"
+
 
     def grab(self):
         if len(self.__q) > 0:
--- src/main.py
+++ src/main.py
@@ -8,6 +8,9 @@ cam.setDaemon(True)
 cam.start()
 prev = None
 img = np.zeros([960,1280,3],dtype=np.uint8)
+
+out_pipe = "appsrc ! video/x-raw,width=1280,height=960,framerate=10/1 ! videoconvert ! timeoverlay xpad=100 ypad=100 ! autovideosink sync=false"
+out = cv2.VideoWriter(out_pipe, 0, 10.0, (1280,960))
 while(True): 
     ret, frame = cam.grab()
     if not ret:
@@ -15,12 +18,15 @@ while(True):
     # prev = frame.copy()
     img[0:480,0:640,:] = frame
     img[480:,640:,:] = frame
+    out.write(img)
     if ret:
-        cv2.imshow('Original', img) 
+        pass
+        # cv2.imshow('Original', img) 
   
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
     time.sleep(1/10)
 # Close the window / Release webcam 
 cam.release() 
+out.release()
 cv2.destroyAllWindows() 
